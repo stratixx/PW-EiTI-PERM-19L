7c4be87ba914a5cbbe3e6d5695ae5cc355243933
stratixx <darnokusex@wp.pl>
2019-03-31 16:45:10 +0200

    dodane wybieranie źródła dźwięku

--- lab3/audioCaptureInit.m
+++ lab3/audioCaptureInit.m
@@ -1,8 +1,19 @@
-deviceReader = audioDeviceReader('Device', device);
+deviceReader = audioDeviceReader();
+
+disp('Select audio source: ');
+devices = deviceReader.getAudioDevices();
+for n=1:length(devices)
+   disp([num2str(n), ': ', devices{n}]);
+end
+deviceIndex = input(['deviceIndex[1..', num2str(length(devices)), '] = ']);
+deviceReader.Device = deviceReader.getAudioDevices{deviceIndex};
+
 setup(deviceReader);
+Fs = deviceReader.SampleRate;
 fileWriter = dsp.AudioFileWriter(...
     'mySpeech.wav',...
     'FileFormat','WAV');
+
 minValue = 2e-05; %ponizej tej wartosci uznawane za szum 
 %byc moze do zmiany na innym mikrofonie
 minSamples = floor(0.7*deviceReader.SamplesPerFrame);
